d989a77aba0693750b4274ef3f07cb715a75faf9
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-02-16 14:43:44 -0300

    :tada: Begin Project

---
+++ src/main/java/br/com/shapeup/ShapeUpApplication.java
@@ -0,0 +1,13 @@
+package br.com.shapeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/shapeup/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.shapeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
