7dcc41910c203be1614243eceac50b3e0d3d4b27
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-02-20 21:01:42 -0300

    healthcheck

---
+++ src/main/java/br/com/shapeup/adapters/web/TesteController.java
@@ -0,0 +1,13 @@
+package br.com.shapeup.adapters.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "UP";
+    }
+}
