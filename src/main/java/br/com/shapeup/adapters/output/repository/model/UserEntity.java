3bf270d99c0ca21494fb7f8adeb7a9bc8bfd3422
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-02-27 03:07:39 -0300

    :recycle: Removed @Data annotation and added @Getter @Setter for best performance

--- src/main/java/br/com/shapeup/adapters/output/repository/model/UserEntity.java
+++ src/main/java/br/com/shapeup/adapters/output/repository/model/UserEntity.java
@@ -1,32 +1,37 @@
 package br.com.shapeup.adapters.output.repository.model;
 
+import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_user")
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private UUID id = UUID.randomUUID();
     @Column
     private String name;
     @Column
     private String lastName;
+
     @Column(unique = true)
     private String email;
+
     @Column(unique = true)
     private String cellPhone;
+
     @Column
     private String password;
 }
