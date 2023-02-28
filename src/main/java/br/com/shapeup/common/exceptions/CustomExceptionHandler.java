c172aa674b545deb7c5ca3b9d4ba02bfeb12fb4e
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-02-28 10:06:04 -0300

    :passport_control: Created new exception user exists by email

--- src/main/java/br/com/shapeup/common/exceptions/CustomExceptionHandler.java
+++ src/main/java/br/com/shapeup/common/exceptions/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.shapeup.common.exceptions;
 
+import br.com.shapeup.common.exceptions.user.UserExistsByEmailException;
 import br.com.shapeup.common.exceptions.user.UserNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,24 +17,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException exception, HttpHeaders headers,
-            HttpStatus status, WebRequest request
-    ) {
-        List<String> errors = exception.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(fieldError -> fieldError.getDefaultMessage())
-                .collect(Collectors.toList());
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> errors = exception.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> fieldError.getDefaultMessage()).collect(Collectors.toList());
 
         var apiErrorMessage = new ApiErrorMessage(status, errors);
         return new ResponseEntity<>(apiErrorMessage, apiErrorMessage.getHttpStatus());
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> handleUserNotFoundException(
-            UserNotFoundException exception, WebRequest request
-    ) {
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception, WebRequest request) {
+        var apiErrorMessage = new ApiErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+        return new ResponseEntity<>(apiErrorMessage, new HttpHeaders(), apiErrorMessage.getHttpStatus());
+    }
+
+    @ExceptionHandler(UserExistsByEmailException.class)
+    public ResponseEntity<Object> handleUserExistsByEmailException(UserExistsByEmailException exception, WebRequest request) {
         var apiErrorMessage = new ApiErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
         return new ResponseEntity<>(apiErrorMessage, new HttpHeaders(), apiErrorMessage.getHttpStatus());
     }
