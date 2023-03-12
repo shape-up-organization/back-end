8ff6364455987651aeeef9190a838fbe58b62e21
Andrey-Benicchio <andrey.bizerra@sptech.school>
2023-03-12 19:48:56 -0300

    mutare in altum import and add validation for deleteUserByEmail

--- src/main/java/br/com/shapeup/adapters/output/integration/user/UserPersistenceAdapter.java
+++ src/main/java/br/com/shapeup/adapters/output/integration/user/UserPersistenceAdapter.java
@@ -4,6 +4,7 @@ import br.com.shapeup.adapters.output.repository.jpa.UserRepositoryJpa;
 import br.com.shapeup.adapters.output.repository.mapper.UserMapper;
 import br.com.shapeup.adapters.output.repository.model.UserEntity;
 import br.com.shapeup.common.exceptions.user.UserExistsByEmailException;
+import br.com.shapeup.common.exceptions.user.UserNotFoundException;
 import br.com.shapeup.core.domain.user.User;
 import br.com.shapeup.core.ports.output.UserPersistanceOutput;
 import lombok.AllArgsConstructor;
@@ -50,6 +51,10 @@ public class UserPersistenceAdapter implements UserPersistanceOutput {
     @Override
     @Transactional
     public void deleteByEmail(String email) {
-     userRepositoryJpa.deleteByEmail(email);
+
+        if (!userRepositoryJpa.existsByEmail(email)) {
+        throw new UserNotFoundException(email);
+        }
+            userRepositoryJpa.deleteByEmail(email);
     }
 }
