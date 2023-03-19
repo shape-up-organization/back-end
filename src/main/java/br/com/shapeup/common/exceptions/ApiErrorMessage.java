771fedd22680d0046234451b17830fe7eee8e501
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-03-19 02:30:21 -0300

    :sparkles: Added status code on exceptions

--- src/main/java/br/com/shapeup/common/exceptions/ApiErrorMessage.java
+++ src/main/java/br/com/shapeup/common/exceptions/ApiErrorMessage.java
@@ -9,19 +9,22 @@ import org.springframework.http.HttpStatus;
 @Getter
 @Setter
 public class ApiErrorMessage {
+    private int statusCode;
     private HttpStatus httpStatus;
     private String path;
     private List<String> errors;
 
-    public ApiErrorMessage(HttpStatus httpStatus, String path, List<String> errors) {
+    public ApiErrorMessage(int statusCode, HttpStatus httpStatus, String path, List<String> errors) {
         super();
+        this.statusCode = statusCode;
         this.httpStatus = httpStatus;
         this.path = path;
         this.errors = errors;
     }
 
-    public ApiErrorMessage(HttpStatus httpStatus, String path, String error) {
+    public ApiErrorMessage(int statusCode, HttpStatus httpStatus, String path, String error) {
         super();
+        this.statusCode = statusCode;
         this.httpStatus = httpStatus;
         this.path = path;
         this.errors = Arrays.asList(error);
--- src/main/java/br/com/shapeup/common/exceptions/CustomExceptionHandler.java
+++ src/main/java/br/com/shapeup/common/exceptions/CustomExceptionHandler.java
@@ -10,6 +10,7 @@ import br.com.shapeup.common.exceptions.user.UserInvalidNameException;
 import br.com.shapeup.common.exceptions.user.UserInvalidPasswordException;
 import br.com.shapeup.common.exceptions.user.UserNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import org.apache.coyote.Request;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
@@ -26,27 +27,37 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
-                                                                  HttpHeaders headers, HttpStatus status, HttpServletRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatus status,
+            HttpServletRequest request
+    ) {
         List<String> errors = exception.getBindingResult().getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
 
-        var apiErrorMessage = new ApiErrorMessage(status, request.getRequestURI(), errors);
+        var apiErrorMessage = new ApiErrorMessage(HttpServletResponse.SC_BAD_REQUEST, status, request.getRequestURI(), errors);
         request.getContextPath();
         return new ResponseEntity<>(apiErrorMessage, apiErrorMessage.getHttpStatus());
     }
 
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception, HttpServletRequest request) {
-        var apiErrorMessage = new ApiErrorMessage(HttpStatus.NOT_FOUND, request.getRequestURI(), exception.getMessage());
+    public ResponseEntity<Object> handleUserNotFoundException(
+            UserNotFoundException exception,
+            HttpServletRequest request
+    ) {
+        var apiErrorMessage = new ApiErrorMessage(HttpServletResponse.SC_NOT_FOUND, HttpStatus.NOT_FOUND, request.getRequestURI(), exception.getMessage());
         return new ResponseEntity<>(apiErrorMessage, new HttpHeaders(), apiErrorMessage.getHttpStatus());
     }
 
     @ExceptionHandler(UserExistsByEmailException.class)
-    public ResponseEntity<Object> handleUserExistsByEmailException(UserExistsByEmailException exception,
-                                                                   HttpServletRequest request) {
-        var apiErrorMessage = new ApiErrorMessage(HttpStatus.CONFLICT, request.getRequestURI(), exception.getMessage());
+    public ResponseEntity<Object> handleUserExistsByEmailException(
+            UserExistsByEmailException exception,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        var apiErrorMessage = new ApiErrorMessage(HttpServletResponse.SC_CONFLICT, HttpStatus.CONFLICT, request.getRequestURI(), exception.getMessage());
         return new ResponseEntity<>(apiErrorMessage, new HttpHeaders(), apiErrorMessage.getHttpStatus());
     }
 
@@ -60,9 +71,12 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
             UserInvalidNameException.class,
             UserExistsByCellPhoneException.class
     })
-    public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException exception,
-                                                                  HttpServletRequest request) {
-        var apiErrorMessage = new ApiErrorMessage(HttpStatus.BAD_REQUEST, request.getRequestURI(),exception.getMessage());
+    public ResponseEntity<Object> handleUsernameNotFoundException(
+            UsernameNotFoundException exception,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        var apiErrorMessage = new ApiErrorMessage(HttpServletResponse.SC_BAD_REQUEST, HttpStatus.BAD_REQUEST, request.getRequestURI(), exception.getMessage());
         return new ResponseEntity<>(apiErrorMessage, new HttpHeaders(), apiErrorMessage.getHttpStatus());
     }
 }
