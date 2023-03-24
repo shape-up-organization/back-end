ada5d9dfe57f55bb9ef7899eba6d5f3b4ac626dd
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-03-24 01:46:13 -0300

    :bug: role constuctor

--- src/main/java/br/com/shapeup/adapters/output/repository/model/user/Role.java
+++ src/main/java/br/com/shapeup/adapters/output/repository/model/user/Role.java
@@ -1,6 +1,5 @@
 package br.com.shapeup.adapters.output.repository.model.user;
 
-import br.com.shapeup.adapters.output.repository.model.user.UserEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,12 +7,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import java.util.List;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 @Entity
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Role {
 
     public Role(String role) {
@@ -28,5 +33,19 @@ public class Role {
     private String role;
 
     @ManyToMany(mappedBy = "roles")
+    @ToString.Exclude
     private List<UserEntity> users;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Role role = (Role) o;
+        return getId() != null && Objects.equals(getId(), role.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
