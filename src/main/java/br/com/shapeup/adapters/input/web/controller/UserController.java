76d2c7e92a74c90870a90c79b6d1b996560f457e
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-03-27 11:24:54 -0300

    :recycle: Refactoring picture upload feature

--- src/main/java/br/com/shapeup/adapters/input/web/controller/UserController.java
+++ src/main/java/br/com/shapeup/adapters/input/web/controller/UserController.java
@@ -7,25 +7,19 @@ import br.com.shapeup.adapters.input.web.controller.request.user.UserCellphoneRe
 import br.com.shapeup.adapters.input.web.controller.request.user.UserLastNameRequest;
 import br.com.shapeup.adapters.input.web.controller.request.user.UserNameRequest;
 import br.com.shapeup.adapters.input.web.controller.request.user.UserPasswordRequest;
-import br.com.shapeup.core.ports.input.UserPersistanceInput;
+import br.com.shapeup.core.ports.input.user.UserPersistanceInput;
 import br.com.shapeup.security.service.JwtService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import java.net.URL;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -122,15 +116,5 @@ public class UserController {
         userPersistanceInput.deleteByEmail(email);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT.value()).build();
-    }
-
-    @PostMapping("/picture")
-    public ResponseEntity<Map<String, URL>> uploadPicture(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
-        String token = request.getHeader("Authorization").substring(7);
-
-        var uploadPictureProfile = userPersistanceInput.uploadPicture(file, token);
-        var urlUserPictureProfileResponse = Map.of("uploadPictureProfile-picture-profile", uploadPictureProfile);
-
-        return ResponseEntity.status(HttpStatus.OK.value()).body(urlUserPictureProfileResponse);
     }
 }
