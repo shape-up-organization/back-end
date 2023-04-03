1a1ebd1159ed150b93b64eecd5737d21ef2c66a0
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-04-03 08:19:23 -0300

    :sparkles: Added ennotation @EnableMongoRepositories

--- src/main/java/br/com/shapeup/ShapeUpApplication.java
+++ src/main/java/br/com/shapeup/ShapeUpApplication.java
@@ -3,9 +3,11 @@ package br.com.shapeup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableMongoRepositories
 public class ShapeUpApplication {
 
     public static void main(String[] args) {
