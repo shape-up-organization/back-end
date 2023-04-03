09e2470879da0d0b190e34e0f752ef2ee04bb33d
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-04-03 08:19:09 -0300

    :recycle: Refactoring user factory

--- src/test/java/br/com/shapeup/factories/UserFactory.java
+++ src/test/java/br/com/shapeup/factories/UserFactory.java
@@ -5,11 +5,9 @@ import br.com.shapeup.core.domain.user.CellPhone;
 import br.com.shapeup.core.domain.user.Email;
 import br.com.shapeup.core.domain.user.Password;
 import br.com.shapeup.core.domain.user.User;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.UUID;
 
 public class UserFactory {
     private static UserFactory instace = null;
@@ -25,7 +23,7 @@ public class UserFactory {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate localDate = LocalDate.parse("05/10/2000", formatter);
         User user = User.newUser("Iara", "Alves", "iarinha", Email.create("iara@gmail.com"), CellPhone.create("11983323932"),
-                Password.create("iara123"), Birth.create(localDate));
+                Password.create("iara123"), Birth.create(localDate), UUID.randomUUID());
         return user;
     }
 }
