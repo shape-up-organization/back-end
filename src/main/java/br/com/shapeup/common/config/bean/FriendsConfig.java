804d4aec02eb4effdfda8875c8bcd9027cec6695
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-04-03 08:13:31 -0300

    :sparkles: Friendship on application layer and your bean configuration

--- src/main/java/br/com/shapeup/common/config/bean/FriendsConfig.java
+++ src/main/java/br/com/shapeup/common/config/bean/FriendsConfig.java
@@ -1,9 +1,9 @@
 package br.com.shapeup.common.config.bean;
 
+import br.com.shapeup.adapters.output.integration.friend.FindFriendshipAdapter;
+import br.com.shapeup.adapters.output.integration.friend.FriendshipAdapter;
 import br.com.shapeup.adapters.output.integration.user.FindUserAdapter;
-import br.com.shapeup.adapters.output.integration.user.FriendsAdapter;
-import br.com.shapeup.adapters.output.integration.user.UserPersistenceAdapter;
-import br.com.shapeup.core.usecase.user.FriendsUsecase;
+import br.com.shapeup.core.usecase.friend.FriendshipUsecase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +11,15 @@ import org.springframework.context.annotation.Configuration;
 public class FriendsConfig {
 
     @Bean
-    public FriendsUsecase friendsUsecase(FriendsAdapter friendsAdapter, FindUserAdapter findUserAdapter) {
-        return new FriendsUsecase(friendsAdapter, findUserAdapter);
+    public FriendshipUsecase friendsUsecase(
+            FriendshipAdapter friendsAdapter,
+            FindUserAdapter findUserAdapter,
+            FindFriendshipAdapter findFriendshipAdapter
+    ) {
+        return new FriendshipUsecase(
+                friendsAdapter,
+                findUserAdapter,
+                findFriendshipAdapter
+        );
     }
-
 }
