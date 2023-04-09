5b1174da76f13d2a03e356e56a9f980fea636d52
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-04-08 21:18:38 -0300

    :sparkles: Create ports for get all friendship

--- src/main/java/br/com/shapeup/core/ports/output/friend/FriendshipOutput.java
+++ src/main/java/br/com/shapeup/core/ports/output/friend/FriendshipOutput.java
@@ -2,9 +2,12 @@ package br.com.shapeup.core.ports.output.friend;
 
 import br.com.shapeup.core.domain.friend.FriendshipRequest;
 import br.com.shapeup.core.domain.user.User;
+import java.util.List;
 
 public interface FriendshipOutput {
     FriendshipRequest sendFriendRequest(User currentUser, User newFriend);
 
     FriendshipRequest acceptFriendRequest(User currentUser, User newFriend);
+
+    List<User> getAllFriendship(User currentUser);
 }
