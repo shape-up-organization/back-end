1beb3a8038509b9ddd89074c91b165f704b93365
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-04-09 22:39:12 -0300

    :bug: Fix bug private-message

--- src/main/java/br/com/shapeup/common/config/websocket/WebSocketConfig.java
+++ src/main/java/br/com/shapeup/common/config/websocket/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package br.com.shapeup.common.config.websocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
