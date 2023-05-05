b9968091abe8abbe6223c3812a99a7a6586483a5
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-05-05 16:59:11 -0300

    feature(friendship repository): - added method for delete friendship request by and and new finders
    Closes #129

--- src/main/java/br/com/shapeup/adapters/output/repository/jpa/friend/FriendshipMongoRepository.java
+++ src/main/java/br/com/shapeup/adapters/output/repository/jpa/friend/FriendshipMongoRepository.java
@@ -1,6 +1,7 @@
 package br.com.shapeup.adapters.output.repository.jpa.friend;
 
 import br.com.shapeup.adapters.output.repository.model.friend.FriendshipRequestDocument;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FriendshipMongoRepository extends MongoRepository<FriendshipRequestDocument, String> {
 
-    void deleteByUsernameSenderAndUsernameReceiver(String usernameSender,String usernameReceiver);
+    void deleteById(String id);
 
     Optional<FriendshipRequestDocument> findByUsernameSenderAndUsernameReceiver(String usernameSender, String usernameReceiver);
+
+    List<FriendshipRequestDocument> findAllByUsernameSenderAndUsernameReceiver(String usernameSender, String usernameReceiver);
+
+    Optional<FriendshipRequestDocument> findByUsernameSenderAndUsernameReceiverAndAccepted(String usernameSender, String usernameReceiver, Boolean accepted);
 }
