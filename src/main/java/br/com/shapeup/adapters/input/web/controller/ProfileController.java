043e57fea2acac94f630b0bc16b353228a3728d3
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-05-09 23:27:35 -0300

    fix cors error

--- src/main/java/br/com/shapeup/adapters/input/web/controller/ProfileController.java
+++ src/main/java/br/com/shapeup/adapters/input/web/controller/ProfileController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/profiles")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ProfileController {
 
