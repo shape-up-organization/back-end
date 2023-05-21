75844403bc36a3237c5b71a1ead13d6d4047f0da
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-05-20 23:44:18 -0300

    add not null on training column

--- src/main/java/br/com/shapeup/adapters/output/repository/model/quest/ExerciseEntity.java
+++ src/main/java/br/com/shapeup/adapters/output/repository/model/quest/ExerciseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.UUID;
@@ -29,6 +30,6 @@ public class ExerciseEntity {
     @Column(nullable = false)
     private String exercise;
     @ManyToOne
-    @Column(nullable = false)
+    @JoinColumn(name = "training_id", nullable = false)
     private TrainingEntity training;
 }
