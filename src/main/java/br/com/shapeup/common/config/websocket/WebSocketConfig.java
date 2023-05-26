5dd4a42b3c06b4f17efb65f9a2818fd7ff874b84
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-05-25 23:55:36 -0300

    return to ws registry

--- src/main/java/br/com/shapeup/common/config/websocket/WebSocketConfig.java
+++ src/main/java/br/com/shapeup/common/config/websocket/WebSocketConfig.java
@@ -11,8 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/shapeup").setAllowedOriginPatterns("*").withSockJS();
-        registry.addEndpoint("/shapeup").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
     }
 
     @Override
