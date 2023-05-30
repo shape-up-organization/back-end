309fcb5f78031ad9b18578c7be202e006966b677
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-05-29 23:01:56 -0300

    update timezone

--- src/main/java/br/com/shapeup/ShapeUpApplication.java
+++ src/main/java/br/com/shapeup/ShapeUpApplication.java
@@ -1,5 +1,6 @@
 package br.com.shapeup;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ShapeUpApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
         SpringApplication.run(ShapeUpApplication.class, args);
     }
 
