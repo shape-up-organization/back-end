da3a892885a754888ba9cceb26733417e46881b1
gabs <gabrieldasilvaprivado1@gmail.com>
2023-11-28 20:39:21 -0300

    generate only numbers in code

--- src/main/java/br/com/shapeup/core/usecase/verification/VerificationCodeGenerator.java
+++ src/main/java/br/com/shapeup/core/usecase/verification/VerificationCodeGenerator.java
@@ -1,13 +1,9 @@
 package br.com.shapeup.core.usecase.verification;
 
-import java.util.UUID;
-
 public class VerificationCodeGenerator {
 
     public static String generate() {
-        UUID uuid = UUID.randomUUID();
-        String uuidString = uuid.toString().replace("-", "");
-        Integer startIndex = uuidString.length() - 6;
-        return uuidString.substring(startIndex);
+        int randomNumber = (int) (Math.random() * 9000) + 1000;
+        return String.valueOf(randomNumber);
     }
 }
