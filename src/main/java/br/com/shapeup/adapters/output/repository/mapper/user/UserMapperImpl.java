7889878ce3cc5135f046dfc04cee826ceecc36b8
gabs <gabrieldasilvaprivado1@gmail.com>
2023-12-02 12:37:00 -0300

    fix update user null biography

--- src/main/java/br/com/shapeup/adapters/output/repository/mapper/user/UserMapperImpl.java
+++ src/main/java/br/com/shapeup/adapters/output/repository/mapper/user/UserMapperImpl.java
@@ -64,6 +64,7 @@ public class UserMapperImpl implements UserMapper {
                 .originalPassword(user.getOriginalPassword())
                 .isActive(true)
                 .biography(user.getBiography())
+                .profilePicture(user.getProfilePicture())
                 .build();
     }
 
